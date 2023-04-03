48098d117a1f3d8a44a256981ce8169fdc634e2f
ovins <ovins@outlook.com>
2023-04-03 15:13:53 +0800

    feat:添加实时时间

--- browser/src/components/LoginInfo.tsx
+++ browser/src/components/LoginInfo.tsx
@@ -1,31 +1,34 @@
 import React, {FC} from "react";
 import {Button} from "antd";
 import {useNavigate} from "react-router-dom";
+import WidgetTime from "./WidgetTime";
 
 /**
  * 显示登录用户的信息,如果还没登录,也会跳转到登录界面
  */
 const App: FC = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const Logout = () => {
-    sessionStorage.removeItem("userinfo");
-    sessionStorage.removeItem("userrole");
-    sessionStorage.removeItem("token");
-    localStorage.removeItem("token");
-    navigate("/login");
-  }
-  const userinfo = JSON.parse(sessionStorage.getItem("userinfo")+"");
+    const Logout = () => {
+        sessionStorage.removeItem("userinfo");
+        sessionStorage.removeItem("userrole");
+        sessionStorage.removeItem("token");
+        localStorage.removeItem("token");
+        navigate("/login");
+    }
+    const userinfo = JSON.parse(sessionStorage.getItem("userinfo") + "");
 
-  return (
-    <>
-      {sessionStorage.getItem("token")!=null &&
+    return (
         <>
-            <h3>欢迎你,{userinfo!=null&&userinfo.truename}</h3>
-            <Button type={"primary" } danger onClick={Logout}>注销</Button>
+            {sessionStorage.getItem("token") != null &&
+                <>
+                    <WidgetTime/>
+                    <h3>欢迎你,{userinfo != null && userinfo.truename}</h3>
+                    <Button type={"primary"} danger onClick={Logout}>注销</Button>
+
+                </>
+            }
         </>
-      }
-    </>
-  );
+    );
 }
 export default App;
---
+++ browser/src/components/WidgetTime.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
+const weeks = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+
+const App = () => {
+    const [renderKey, setRenderKey] = useState(1);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setRenderKey(Math.random());
+        }, 1000);
+        // 组件卸载清除定时器
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        // 获取当前时间 年-月-日 时:分:秒  moment().format('YYYY-MM-DD HH:mm:ss')
+        // 获取星期几，其中星期日为0、星期六为6  moment().day()
+        <div style={{backgroundColor:'#ffffb8',width:'160px',margin:'20px',border:'solid #faad14 2px',borderRadius:'10px'}}>
+            <div style={{fontSize:'16px'}}>
+                {moment().format('YYYY-MM-DD')}&nbsp;{weeks[moment().day()]}
+            </div>
+            <div style={{fontSize:'32px',color:'#0958d9'}}>{moment().format('HH:mm:ss')}</div>
+        </div>
+    );
+};
+export default App;
