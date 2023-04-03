b3ae94fe728b613a984f417c4509b8d3175808a9
ovins <ovins@outlook.com>
2023-04-03 14:41:15 +0800

    feat:部门管理模块功能完善

--- browser/src/views/org/DeptMgr.tsx
+++ browser/src/views/org/DeptMgr.tsx
@@ -1,61 +1,51 @@
 import {Button, Col, Form, Input, Pagination, PaginationProps, Popconfirm, Row, Select, Table, Typography} from "antd";
-import React, {FC, useEffect, useState} from "react";
+import React, {FC, useEffect, useMemo, useState} from "react";
 import "../system/System.module.css"
 import {EditableCellProps, IPage} from "../common";
-import {
-    createDept,
-    eraseDept,
-    getDept,
-    getDeptPage,
-    getDeptPageByStatus,
-    updateDept
-} from "../../api/org";
+import {createDept, eraseDept, getDeptPage, updateDept} from "../../api/org";
 import {DeptItem} from "../../model/org";
+import {getUserList, getUserListLike} from "../../api/user";
+import {UserItem} from "../../model/user";
+// @ts-ignore
+import debounce from 'lodash/debounce';
 
 
 const App: FC = () => {
     const [total, setTotal] = useState(0);
     const [pageNumber, setPageNumber] = useState(1);
     const [pageSize, setPageSize] = useState(10);
-    const [data, setData] = useState([]);
-    const [dept, setDept] = useState([] as Array<DeptItem>);
-    const [DeptItem, setDeptItem] = useState({} as DeptItem);
-    const [status, setStatus] = useState('Y');
-    const [hasNew, setHasNew] = useState(false);
+
+    const [data, setData] = useState([] as Array<DeptItem>); //表格当前分页数据
+
+    const [newDept, setNewDept] = useState({status: 'Y'} as DeptItem); //新建部门
+
+    const [mgrUser, setMgrUser] = useState([] as Array<UserItem>);//当前页需要的部门主管信息
+
+    const [searchUserList, setSearchUserList] = useState([] as Array<UserItem>);//搜索用户列表
+
+    const [hasNew, setHasNew] = useState(false); //刷新数据
+
 
     useEffect(() => {
         let param: IPage = {
             current: pageNumber,
             size: pageSize,
         };
-        getDeptPage(param)
-            .then(res => {
-                setTotal(res.total)
-                setData(res.records)
-                setHasNew(false)
-            })
-    }, [pageNumber, pageSize, DeptItem, hasNew]);
-
-    useEffect(() => {
-        let param = {
-            current: pageNumber,
-            size: pageSize,
-            status: status,
-        };
-
-        getDeptPageByStatus(param)
-            .then(res => {
-                setTotal(res.total)
-                setData(res.records)
-                setHasNew(false)
-            })
-    }, [pageNumber, pageSize, status, hasNew]);
-
-    useEffect(() => {
-        getDept().then((res) => {
-            setDept(res)
-        })
-    }, [hasNew])
+        getDeptPage(param).then(res => {
+            //获得所有的部门主管的id
+            const mgrList = res.records.map((item: DeptItem) => item.uid);
+            if (mgrList.length > 0) {
+                //通过部门主管门的id找信息
+                getUserList(mgrList).then((res: any) => {
+                    setMgrUser(res)//用于显示
+                    setSearchUserList(res)//用户搜索框默认选项
+                })
+            }
+            setData(res.records)
+            setTotal(res.total)
+        });
+        setHasNew(false)
+    }, [pageNumber, pageSize, hasNew]);
 
     const onChange: PaginationProps['onChange'] = (pageNumber, pageSize) => {
         setPageNumber(pageNumber);
@@ -126,13 +116,24 @@ const App: FC = () => {
             render: (text: any) => <a>{text}</a>,
         },
         {
+            title: '部门主管',
+            dataIndex: 'uid',
+            key: 'uid',
+            align: 'center',
+            editable: true,
+            render: (item: number) => {
+                const tempd = mgrUser.find((i) => i.id === item)
+                return tempd != null ? tempd.truename : "";
+            }
+        },
+        {
             title: '所属部门',
             dataIndex: 'did',
             key: 'did',
+            align: 'center',
             editable: true,
-
             render: (item: number) => {
-                const tempd = dept.find((i) => i.id === item)
+                const tempd = data.find((i) => i.id === item)
                 return tempd != null ? tempd.name : "";
             }
         },
@@ -140,13 +141,14 @@ const App: FC = () => {
             title: '状态',
             dataIndex: 'status',
             key: 'status',
+            align: 'center',
             editable: true,
             render: (text: string) => text === 'Y' ? "启用" : text === 'N' ? "停用" : "删除",
-
         },
         {
             title: '操作',
             dataIndex: 'operation',
+            align: 'center',
             render: (_: any, record: Partial<DeptItem>) => {
                 const editable = isEditing(record as DeptItem);
                 return editable ? (
@@ -168,6 +170,23 @@ const App: FC = () => {
 
     ];
 
+    const [openSelect, setOpenSelect] = useState(false);//控制列表是否默认展开
+
+    const [fetching, setFetching] = useState(false);
+    //防抖用户搜索,时间内只能发送一个请求
+    const debounceFetcher = useMemo(() => {
+        const loadOptions = (username: string) => {
+            setFetching(true);
+
+            getUserListLike(username).then((res) => {
+                setSearchUserList(res);
+                setFetching(false);
+                setOpenSelect(true);//搜索完成,刷新列表,会关闭展开项,这里主动展开
+            });
+        };
+        return debounce(loadOptions, 1000);//loadOptions上次调用后,时间超过才能再次调用
+    }, []);
+
     const EditableCell: React.FC<EditableCellProps> = ({
                                                            editing,
                                                            dataIndex,
@@ -181,11 +200,25 @@ const App: FC = () => {
         let inputNode;
 
         switch (dataIndex) {
+            case 'uid':
+                inputNode =
+                    <Select style={{width: 120}} showSearch placeholder="姓名搜索"
+                            onSearch={debounceFetcher}
+                            defaultOpen={openSelect}
+                            onSelect={() => {
+                                setOpenSelect(false);
+                            }}
+                            options={
+                                searchUserList.map((item) => {
+                                    return {key: item.id, value: item.id, label: item.truename}
+                                })}>
+                    </Select>
+                break;
             case 'did':
                 inputNode = <Select style={{width: 120}}>
                     <option value={undefined}>{'无'}</option>
                     {
-                        dept.map((item) => {
+                        data.map((item) => {
                             if (item.id !== record.id && item.did !== record.id) {//父级部门不能是自己,也不能是子部门
                                 return <option key={item.id} value={item.id}>{item.name}</option>
                             }
@@ -225,7 +258,7 @@ const App: FC = () => {
             ...col,
             onCell: (record: DeptItem) => ({
                 record,
-                inputType: col.dataIndex === 'code' ? 'number' : 'text',
+                inputType: 'text',
                 dataIndex: col.dataIndex,
                 title: col.title,
                 editing: isEditing(record),
@@ -234,51 +267,60 @@ const App: FC = () => {
     });
 
 
-    let tempDept = {status: 'Y'} as DeptItem;
-
     const btnAddOnClick = () => {
-        createDept(tempDept).then((res) => {
-            console.log(res)
-            setDeptItem(tempDept)
+        createDept(newDept).then((res) => {
+            setNewDept({status:'Y'} as DeptItem)
+            setHasNew(true)
         })
     }
-    const btnStatusOnClick = (status: string) => {
-        if (status.length === 0) {
-            setDeptItem(tempDept)
-        } else {
-            setStatus(status)
-        }
-    }
+
 
     return (
         <>
             <Form form={form} component={false}>
                 <Row>
                     <Col><Input onChange={(e) => {
-                        tempDept = {...tempDept, name: e.target.value};
+                        setNewDept({...newDept, name: e.target.value});
 
                     }} placeholder={"部门名称"} style={{width: 150}}/></Col>
                     <Col>
                         <Select onChange={(e) => {
-                            tempDept = {...tempDept, did: e};
+                            setNewDept({...newDept, did: e});
 
-                        }} placeholder={"所属部门"} style={{width: 100}} options={
-                            dept.map((item) => {
-                                return {key: item.id, label: item.name, value: item.id}
-                            })}/>
+                        }} placeholder={"所属部门"} style={{width: 100}}>
+                            <option value={undefined}>{'无'}</option>
+                            {
+                                data.map((item) => {
+                                    return <option key={item.id} label={item.name} value={item.id}>{item.name}</option>
+                                })
+                            }
+                        </Select>
+                    </Col>
+                    <Col>
+                        <Select style={{width: 120}} showSearch placeholder="部门主管"
+                                onSearch={debounceFetcher}
+                                defaultOpen={openSelect}
+                                onSelect={(e) => {
+                                    setNewDept({...newDept, uid: e});
+                                    setOpenSelect(false);
+                                }}
+                                options={
+                                    searchUserList.map((item) => {
+                                            return {key: item.id, value: item.id, label: item.truename}
+                                        }
+                                    )}>
+                        </Select>
                     </Col>
                     <Col><Button type={"primary"} onClick={btnAddOnClick}>新建</Button></Col>
-                    <Col offset={5}>
-                        <Button style={{width: 120}} onClick={() => btnStatusOnClick("Y")}>仅查看已启用</Button>
-                        <Button style={{width: 120}} onClick={() => btnStatusOnClick("N")}>仅查看已停用</Button>
-                    </Col>
+
                 </Row>
                 <Table components={{
                     body: {
                         cell: EditableCell,
                     },
                 }} size={'middle'} columns={mergedColumns} rowClassName="editable-row" dataSource={data}
-                       pagination={false} bordered/>
+                       pagination={false}
+                       bordered/>
             </Form>
             <Pagination hideOnSinglePage showQuickJumper defaultCurrent={1} total={total} onChange={onChange}/>
         </>
